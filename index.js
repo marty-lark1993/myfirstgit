a829240ba9f47bf1353bf3955933925382be8943
Martin Lark <martin.lark@hotmail.com>
2024-07-22 00:46:53 +1000

    merged

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 let sum = (num1, num2) => {
   return num1 + num2;
 };
 
 console.log("Hello World");
 console.log(sum(5, 10));
-=======
-console.log("i caused a merge conflict");
->>>>>>> main
